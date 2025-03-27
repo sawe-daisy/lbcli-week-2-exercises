5ac1c83eeddbd57efd15ecd28956f14387303747
sawe-daisy <deesawe16@gmail.com>
2025-03-27 08:53:09 +0300

    bitcoin cli commands

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+BITCOIN_CLI="bitcoin-cli -regtest -rpcwallet=btrustwallet"
+
+# if ! $BITCOIN_CLI getwalletinfo &>/dev/null; then
+#   bitcoin-cli -regtest createwallet "btrustwallet"
+# fi
+
+ADDRESS=$($BITCOIN_CLI getnewaddress "" bech32)
+
+# if [ -z "$ADDRESS" ]; then
+#   exit 1
+# fi
+
+$BITCOIN_CLI generatetoaddress 50 "$ADDRESS"
+$BITCOIN_CLI generatetoaddress 50 "$ADDRESS"
+
+echo $ADDRESS
+
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
